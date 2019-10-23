8ed3cee558ce0a7c8f0657856ebb75bd16b25daa
Horacio Diaz <bregadev@gmail.com>
2019-10-23 12:32:46 -0500

    change client request method from form to ajax

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect, \
-url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -16,21 +15,25 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+
 db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.form.get('description', '')
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return redirect(url_for('index'))
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description': todo.description
+    })
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all()
-    )
+    return render_template('index.html', data=Todo.query.all())
+
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
