bacc407154022a8b4e5261045730c8f5e94b46d3
Horacio Diaz <bregadev@gmail.com>
2019-10-25 18:22:28 -0500

    imported and instantiated Migrate object from flask-Migrate

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask import abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:docker123@localhost:5432/todo'
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
