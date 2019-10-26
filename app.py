1ba884f4115624de4cb4307d14be24b46589eeb9
Horacio Diaz <bregadev@gmail.com>
2019-10-26 13:20:38 -0500

    add flask migrate version control to the app

--- app.py
+++ app.py
@@ -16,12 +16,10 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-
-db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
